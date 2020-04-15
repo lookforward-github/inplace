ee6219ca6bc5f0d6ca0e058330d811dcccb08672
Vladislav Teterin <teterin1vl@gmail.com>
2020-04-15 23:36:49 +0900

    Fix data regenerating

--- server/app.py
+++ server/app.py
@@ -1,18 +1,43 @@
-import base64
-import io
-import time
-
 from flask import Flask, render_template, send_file
 import random
-import json
+import io
+import time
+from os import path
+
 app = Flask(__name__)
 
 DATA_SIZE = 1000000
 DATA_FILE = 'inplace.data'
-DATA = []
 
-with open(DATA_FILE, "rb") as f:
-    DATA_RAW = f.read()
+
+def regenerate():
+    global DATA_RAW
+    print('Data regenerating', end='')
+    start = time.time_ns()
+    DATA_RAW = bytearray()
+    for i in range(DATA_SIZE):
+        DATA_RAW.append(random.randrange(0, 256))
+        DATA_RAW.append(random.randrange(0, 256))
+        DATA_RAW.append(random.randrange(0, 256))
+        DATA_RAW.append(255)
+        if i % 100000 == 0:
+            print('.', end='')
+
+    print(' Done!')
+
+    with open(DATA_FILE, 'wb') as f:
+        f.write(DATA_RAW)
+
+    print('Data regenerated in %s ms' % ((time.time_ns() - start) / 1000000))
+
+    return True
+
+
+if not path.exists(DATA_FILE):
+    regenerate()
+
+with open(DATA_FILE, 'rb') as f:
+    DATA_RAW = bytearray(f.read())
 
 
 @app.route('/')
@@ -27,35 +52,17 @@ def get_data():
 
 @app.route('/paint/<int:x>/<int:y>/<int:r>/<int:g>/<int:b>')
 def paint(x, y, r, g, b):
-    global DATA, DATA_DUMP
     offset = (x + 1000 * y) * 4
-    DATA[offset] = r
-    DATA[offset + 1] = g
-    DATA[offset + 2] = b
-    DATA_DUMP = json.dumps(DATA)
+    DATA_RAW[offset] = r
+    DATA_RAW[offset + 1] = g
+    DATA_RAW[offset + 2] = b
     return 'ok'
 
 
 @app.route('/regenerate')
-def reload_data():
-    print("Data regenerating", end='')
-    start = time.time_ns()
-    global DATA
-    DATA = []
-    for i in range(DATA_SIZE):
-        DATA.append(random.randrange(0, 256))
-        DATA.append(random.randrange(0, 256))
-        DATA.append(random.randrange(0, 256))
-        DATA.append(255)
-        if i % 100000 == 0:
-            print(".", end='')
-
-    print(' Done!')
-
-    with open(DATA_FILE, 'wb') as f:
-        f.write(bytearray(DATA))
-
-    return 'Data regenerated in %s ms' % ((time.time_ns() - start) / 1000000)
+def regenerate_data():
+    regenerate()
+    return 'ok'
 
 
 if __name__ == '__main__':
